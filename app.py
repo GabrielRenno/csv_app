d53328a0bdcc2ec7394e5508e77efa7401dff0f9
GabrielRenno <gabriel_renno@outlook.com>
2023-05-14 21:49:41 +0200

    COMMIT

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 from pandasai import PandasAI
 from pandasai.llm.openai import OpenAI
 
-openai_api_key = "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"
+#openai_api_key = "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"
 
 openai_api_key = os.getenv('OPENAI_API_KEY')
 
